5b5667708213f7d865a804f725e10adcffcf3526
Amarok-w <Vadimon4k00073@gmail.com>
2021-11-16 21:44:20 +0300

    made counter completed/uncompleted tasks

--- script.js
+++ script.js
@@ -4,21 +4,63 @@ const tasks = document.querySelector('.tasks-flex');
 
 !!localStorage.idCounter ? "" : localStorage.setItem("idCounter", 0);;
 
-// TODO
-// ! Сделать фильтр выполнено/невыполнено
-// ! Возможность редактировать текст задачи
-// ! Счётчик выполненных/невыполненных задач
+let currentFilter = 0;
+// store the filter type
+// 0 - all
+// 1 - completed
+// 2- uncompleted
 
-function tasksRecovery() {
-  if (localStorage.length > 1) {
-    let regularRule = /task*/;
-    for (let i = 0; i < localStorage.length; i++) {
-      if (regularRule.test(localStorage.key(i))) {
-        const task = JSON.parse(localStorage.getItem(localStorage.key(i)));
-        addTask(task.checked, task.text, task.id, false);
+// TODO
+// ? Сделать фильтр выполнено/невыполнено 
+// ? Счётчик выполненных/невыполненных задач
+// ! Возможность редактировать текст задачи
+
+function tasksRecovery(filter = 0) {
+
+  tasks.innerHTML = '';
+
+  function tasksAdding(type = 0) {
+
+    if (type == 0) {
+      let regularRule = /task*/;
+      for (let i = 0; i < localStorage.length; i++) {
+        if (regularRule.test(localStorage.key(i))) {
+          const task = JSON.parse(localStorage.getItem(localStorage.key(i)));
+          addTask(task.checked, task.text, task.id, false);
+        }
+      }
+    } else if (type == 1) {
+      let regularRule = /task*/;
+      for (let i = 0; i < localStorage.length; i++) {
+        if (regularRule.test(localStorage.key(i))) {
+          const task = JSON.parse(localStorage.getItem(localStorage.key(i)));
+          if (task.checked) {
+            addTask(task.checked, task.text, task.id, false);
+          }
+        }
+      }
+    } else {
+      let regularRule = /task*/;
+      for (let i = 0; i < localStorage.length; i++) {
+        if (regularRule.test(localStorage.key(i))) {
+          const task = JSON.parse(localStorage.getItem(localStorage.key(i)));
+          if (!task.checked) {
+            addTask(task.checked, task.text, task.id, false);
+          }
+        }
       }
     }
+
   }
+
+  if (filter == 0) {
+    if (localStorage.length > 1) {
+      tasksAdding();
+    }
+  } else {
+    tasksAdding(filter);
+  }
+
 }
 
 function lengthCheck(str) {
@@ -33,7 +75,7 @@ function lengthCheck(str) {
 
 function butLogic() {
   addBut.addEventListener("click", () => {
-      addTask(false, taskInput.value, localStorage.idCounter);
+    addTask(false, taskInput.value, localStorage.idCounter);
   })
 }
 
@@ -47,13 +89,13 @@ function inputKeyAdding() {
 
 function addTask(checked, text, id, newTask = true, filter) {
   if (lengthCheck(taskInput.value) || !newTask) {
-  
+
     const taskInfo = {
       checked: checked,
       text: text,
       id: id
     }
-  
+
     const task = document.createElement("div");
     task.className = 'task';
     task.setAttribute("data-id", taskInfo.id)
@@ -79,10 +121,10 @@ function addTask(checked, text, id, newTask = true, filter) {
             
     `
     tasks.append(task);
-  
+
     const taskInfoJson = JSON.stringify(taskInfo);
     localStorage.setItem("task" + taskInfo.id, taskInfoJson);
-  
+
     if (newTask) {
       localStorage.idCounter++;
     }
@@ -116,23 +158,55 @@ function addTask(checked, text, id, newTask = true, filter) {
       thisTaskObject.checked = !thisTaskObject.checked;
       thisTaskObjectJson = JSON.stringify(thisTaskObject);
       localStorage.setItem(storageTaskKey, thisTaskObjectJson);
-      // console.log(thisTaskObject);
+
+
+      tasksRecovery(currentFilter);
     })
   }
 }
 
-
 function taskFilter() {
   const but = document.querySelector('.task-filter__but');
   const content = document.querySelector('.dropdown-content');
+  let completedTasksNum = 0;
+  let uncompletedTasksNum = 0;
+  completeCounter();
+
+  const completedItem = content.childNodes[3];
+  const uncompletedItem = content.childNodes[5];
+
+  if (localStorage.length > 1) {
+    completeCountChanger();
+  }
+
   let state = false;
 
-  let currentFilter = 0;
-  // store the filter type
-  // 0 - all
-  // 1 - completed
-  // 2- uncompleted
-  
+
+  function completeCounter() {
+    completedTasksNum = 0;
+    uncompletedTasksNum = 0;
+
+    let regularRule = /task*/;
+    for (let i = 0; i < localStorage.length; i++) {
+      if (regularRule.test(localStorage.key(i))) {
+        const task = JSON.parse(localStorage.getItem(localStorage.key(i)));
+
+        if (task.checked == true) {
+          completedTasksNum++;
+        } else {
+          uncompletedTasksNum++;
+        }
+      }
+    }
+  }
+
+  function completeCountChanger() {
+    completeCounter();
+    completedItem.innerHTML = "Completed - " + completedTasksNum;
+    uncompletedItem.innerHTML = "Unompleted - " + uncompletedTasksNum;
+  }
+
+  completeCounter();
 
   function hideDropList() {
     but.style.color = '#454545';
@@ -146,6 +220,7 @@ function taskFilter() {
   }
 
   but.addEventListener('click', () => {
+    completeCountChanger();
     state = !state;
     if (state) {
       but.style.color = '#fff';
@@ -164,8 +239,18 @@ function taskFilter() {
   })
 
   content.addEventListener('click', el => {
-    currentFilter = el.target.getAttribute('data-current-filter'); 
-    hideDropList(); 
+    if (el.target.classList.contains('dropdown-content__item')) {
+      currentFilter = el.target.getAttribute('data-current-filter');
+      hideDropList();
+
+      if (currentFilter == 1) {
+        but.innerHTML = 'Completed' + '<span class="fas fa-caret-down"></span>';
+      } else if (currentFilter == 2) {
+        but.innerHTML = 'Uncompleted' + '<span class="fas fa-caret-down"></span>';
+      }
+
+      tasksRecovery(currentFilter);
+    }
   })
 
   document.addEventListener('click', el => {
